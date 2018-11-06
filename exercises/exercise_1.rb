4730b5dc009b2f0fd348eef59c5d30979fa88dd0
MonajiLee <lee.smonica@gmail.com>
2018-11-06 20:10:48 +0000

    Change first store name

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,9 +3,6 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-class Store < ActiveRecord::Base
-end
-
 Burnaby = Store.create annual_revenue: 300000, mens_apparel: true, womens_apparel: true
 Richmond = Store.create annual_revenue: 1260000, womens_apparel: true
 Gastown = Store.create annual_revenue: 190000, mens_apparel: true
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,15 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find(1)
+puts "\nFirst Store"
+p @store1
+
+@store2 = Store.find(2)
+puts "\nSecond Store"
+p @store2
+
+puts "\nFirst Store Name Change"
+@store1.name = "Burnaby"
+@store1.save
+p @store1
