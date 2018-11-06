120604f9506067bc5f3939457916b73c2a1249ff
MonajiLee <lee.smonica@gmail.com>
2018-11-06 21:05:15 +0000

    Move dummy employees to exercise file

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,13 +1,3 @@
 class Employee < ActiveRecord::Base
     belongs_to :store
 end
-
-@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "Don", last_name: "Burks", hourly_rate: 45)
-@store1.employees.create(first_name: "Joel", last_name: "Shinness", hourly_rate: 45)
-@store1.employees.create(first_name: "Sam", last_name: "Meech-Ward", hourly_rate: 45)
-
-@store2.employees.create(first_name: "Jenny", last_name: "Poon", hourly_rate: 60)
-@store2.employees.create(first_name: "Angela", last_name: "Larryant", hourly_rate: 50)
-@store2.employees.create(first_name: "Jay", last_name: "Shevolup", hourly_rate: 45)
-@store2.employees.create(first_name: "Gaurav", last_name: "Dang", hourly_rate: 45)
