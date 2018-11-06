20ab7841f5ff1e108b0737ed0072edc007f57b6b
MonajiLee <lee.smonica@gmail.com>
2018-11-06 20:50:39 +0000

    Calculate total and average revenue of all stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
+@totalrevenue = Store.sum(:annual_revenue);
+puts "\nTotal Revenue"
+p @totalrevenue
 
+@averagerevenue = @totalrevenue / Store.count
+puts "\nAverage Revenue"
+p @averagerevenue
