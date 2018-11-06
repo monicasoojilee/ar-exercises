7e3672d8cbe670c2a2ff25c7f4d501ffea99f256
MonajiLee <lee.smonica@gmail.com>
2018-11-06 19:54:37 +0000

    Create 3 stores in DB

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,9 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+class Store < ActiveRecord::Base
+end
+
+Burnaby = Store.create annual_revenue: 300000, mens_apparel: true, womens_apparel: true
+Richmond = Store.create annual_revenue: 1260000, womens_apparel: true
+Gastown = Store.create annual_revenue: 190000, mens_apparel: true
